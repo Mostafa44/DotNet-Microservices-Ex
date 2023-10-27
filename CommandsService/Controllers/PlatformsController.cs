e599992a8a4fe54959b75451a1e5a1e41ba9ec71
Mostafa  Youssef <mostafayoussef@Mostafas-MacBook-Pro.local>
2023-10-27 22:48:50 +0200

    add the controller and actions for commands-service

--- CommandsService/Controllers/PlatformsController.cs
+++ CommandsService/Controllers/PlatformsController.cs
@@ -1,3 +1,6 @@
+using AutoMapper;
+using CommandsService.Data;
+using CommandsService.Dtos;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CommandsService.Controllers
@@ -6,16 +9,26 @@ namespace CommandsService.Controllers
     [ApiController]
     public class PlatformsController :ControllerBase
     {
-     public PlatformsController()
-     {
-        
-     }   
+        private readonly ICommandRepo _repository;
+        private readonly IMapper _mapper;
+        public PlatformsController(ICommandRepo repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+        [HttpGet]
+        public ActionResult<IEnumerable<PlatformReadDto>> GetPlatforms()
+        {
+            Console.WriteLine("---> Getting Platofrms from the Commands-Service");
+            var platforms= _repository.GetAllPlatforms();
+            return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platforms));
+        }
 
-    [HttpPost]
-     public ActionResult TestInBoundConnections()
-     {
-        Console.WriteLine("---> In Bound Post # command Service");
-        return Ok("Inbound Test of from Inbound controller");
-     }
+        [HttpPost]
+        public ActionResult TestInBoundConnections()
+        {
+            Console.WriteLine("---> In Bound Post # command Service");
+            return Ok("Inbound Test of from Inbound controller");
+        }
     }
 }
